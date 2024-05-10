2f23b74986bdc410da7cf2ed9f153a434615b927
J3nbuu <jenbu3d@gmail.com>
2024-05-10 10:51:21 +0100

    User class +login/logout

--- src/User.js
+++ src/User.js
@@ -1,5 +1,26 @@
 class User {
   // User code here
+  constructor(username, password, age){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedin = false
+  }
+
+  //log in method
+  login(password){
+    if (password === this.password){
+      this.loggedin = true;
+    } else {
+      throw new Error("Incorrect Password")
+    }
+  }
+
+
+  //log out method
+  logout(){
+this.loggedin = false
+  }
 }
 
 module.exports = User
